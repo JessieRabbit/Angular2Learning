7da4fef2b3a84b3a3a3855b4c2207098b5617427
jessie <jessie.lan@Sarbeer.com>
2018-01-23 22:30:04 +0800

    [feauture] 1.add matbutton learn

--- angularLearn/src/app/Shared/Shared.module.ts
+++ angularLearn/src/app/Shared/Shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatButtonToggleModule, MatRippleModule } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
+    MatButtonToggleModule,
+    MatRippleModule,
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatButtonToggleModule,
+    MatRippleModule
   ],
 })
 export class SharedModule { }
